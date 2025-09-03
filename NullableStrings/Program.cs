963d356382e02d5e8315931612dd717c5191d9f5
liakosV <billiak@hotmail.com>
2025-09-03 17:10:00 +0300

    feat: Examples for strings and regex added

---
+++ NullableStrings/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace NullableStrings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string s1;          // non-nullable
+            string? s2 = null;  // nullable
+
+            Console.WriteLine("Please insert s1 and s2");
+            s1 = Console.ReadLine()!;
+            s2 = Console.ReadLine(); // can be null
+
+            if (s2 != null)
+            {
+                if ((s1.Equals("Coding")) && (s2.Equals("Factory")))
+                {
+
+                }
+            }
+        }
+
+        public static string? GetUppercaseString()
+        {
+            string? s1;
+
+            Console.WriteLine("Enter a string");
+            s1 = Console.ReadLine();
+            return s1?.ToUpper();
+        }
+    }
+}
---
+++ RegEx/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Text.RegularExpressions;
+
+namespace RegEx
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = "Hello123";
+            string pattern = @"^[A-Z]+\d+";
+            ExactMatch(input, pattern);
+        }
+
+        public static void ExactMatch(string input, string pattern)
+        {
+            Match match = Regex.Match(input, pattern);
+            if (match.Success)
+            {
+                Console.WriteLine(match.Value);
+            }
+        }
+    }
+}
