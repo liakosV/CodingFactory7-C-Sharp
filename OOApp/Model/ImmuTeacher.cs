a4b25df8780ec42632de56d200e4e851fdf3ca3e
liakosV <billiak@hotmail.com>
2025-09-04 13:31:46 +0300

    feat: OOP examples added

---
+++ OOApp/Model/ImmuTeacher.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOApp.Model
+{
+    /// <summary>
+    /// POCO Class - Plain Old CLR Object
+    /// </summary>
+    internal sealed class ImmuTeacher
+    {
+        public ImmuTeacher()
+        {
+        }
+
+        public int Id { get; }
+        public string? Firstname { get; init; }
+        public string? Lastname { get; init; }
+
+        public ImmuTeacher(int id, string? firstname, string? lastname)
+        {
+            Id = id;
+            Firstname = firstname;
+            Lastname = lastname;
+        }
+    }
+}
---
+++ OOApp/Program.cs
@@ -0,0 +1,26 @@
+﻿using OOApp.Model;
+
+namespace OOApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Teacher alice = new Teacher();
+            Teacher bob = new();
+            var Kostas = new Teacher();
+
+            Teacher anna = new Teacher()  //Object Initializer
+            {
+                Id = 1,
+                Firstname = "Anna",
+                Lastname = "Smith"
+            };
+
+            anna.Firstname = "Anna-Maria";
+            Console.WriteLine(anna.Firstname);
+
+            Teacher adnreas = new Teacher(2, "Andreas", "Doe");
+        }
+    }
+}
